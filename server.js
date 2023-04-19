9baaac824f45761ca1c2e760ddf69fa990fabee7
Kevin Rhode <kevinrhode@hotmail.com>
2023-04-18 20:48:24 -0400

    add:fix 2

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const path = require('path');
 const api = require('./routes/index.js');
-const { cLog } = require('./middleware/clog');
+// const { cLog } = require('./middleware/clog');
 const exp = require('constants');
 const PORT = 5001;
 
 const app = express();
 
 // Import custom middleware, "cLog"
-app.use(cLog);
+// app.use(cLog);
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
